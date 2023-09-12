5a1b6682dfc84ac1d9046a8968a6a58272bed3ee
omgitskuei <57666754+omgitskuei@users.noreply.github.com>
2023-09-12 16:18:44 +0800

    Restart project based on MUI example

--- todoapp/server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login } from "../controllers/auth.js";
-
-const router = express.Router();
-
-router.post("/login", login);   // this is /auth/login
-
-export default router;
