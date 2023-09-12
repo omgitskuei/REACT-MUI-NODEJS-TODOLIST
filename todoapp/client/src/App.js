ecba1e1cc79edc4fc569a8cda8d83a7a0498382c
omgitskuei <57666754+omgitskuei@users.noreply.github.com>
2023-09-12 12:48:08 +0800

    Update App.js

--- todoapp/client/src/App.js
+++ todoapp/client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+// import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline } from "@mui/material";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+    <div className="app">
+      <BrowserRouter>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
